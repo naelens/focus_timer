0ba34357f83ab6e05f024a1a9cb36514da69cbb1
naelem <ellenxxy19@gmail.com>
2025-02-14 17:52:37 -0300

    [FEAT] - Header component has been completed

--- src/Router.tsx
+++ src/Router.tsx
@@ -2,7 +2,6 @@ import { Routes, Route } from 'react-router-dom'
 import { Home } from './pages/Home'
 import { History } from './pages/History'
 import { DefaultLayout } from './layouts/DefaultLayout'
-
 export function Router(){
     return (
         <Routes>
